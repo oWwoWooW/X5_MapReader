b7ead089f760479858ff46a4ffe243b36ad20b39
oWwoWooW <39463621+oWwoWooW@users.noreply.github.com>
2018-09-21 18:01:05 +0800

    三模式完成
    修正若干错误
    不再替换note_type关键字
    传出参数放于Map_out中

--- Read_Xml.m
+++ Read_Xml.m
@@ -38,9 +38,11 @@ function status = Read_Xml(XML_file_addr)
         end
         Info_Martix_cell{1, i} = string(Select_Node.item(0).getTextContent);
     end
-    
+    %建立table
     Info_Martix_table = cell2table(Info_Martix_cell, 'VariableNames', Info_KeyWords_cell);
+    %匹配BgmId
     Info_Martix_table.BgmId = regexp(Info_Martix_table.FilePath, '\d+', 'match');
+    %定位Note
     Note_Nodes = XML_file.getElementsByTagName('NoteInfo');
     Note_Nodes = Note_Nodes.item(0);
     Note_Nodes = Note_Nodes.getElementsByTagName('Note');
@@ -55,13 +57,14 @@ function status = Read_Xml(XML_file_addr)
         'Type' 'Track' 'ID' 'Son' 'EndArea'};
     
     Not_Typ_id = find((Note_Attr_KeyWords == 'note_type') ==1);
-    End_Bar_id = (Note_Attr_KeyWords == 'EndBar') ==1;
-    End_Pos_id = (Note_Attr_KeyWords == 'EndPos') ==1;
-    Str_Bar_id = (Note_Attr_KeyWords == 'Bar') ==1;
-    Str_Pos_id = (Note_Attr_KeyWords == 'Pos') ==1;
-    End_Tra_id = (Note_Attr_KeyWords == 'end_track') ==1;
-    Fro_Tra_id = (Note_Attr_KeyWords == 'from_track') ==1;
+    End_Bar_id = find((Note_Attr_KeyWords == 'EndBar') ==1);
+    End_Pos_id = find((Note_Attr_KeyWords == 'EndPos') ==1);
+    Str_Bar_id = find((Note_Attr_KeyWords == 'Bar') ==1);
+    Str_Pos_id = find((Note_Attr_KeyWords == 'Pos') ==1);
+    End_Tra_id = find((Note_Attr_KeyWords == 'end_track') ==1);
+    Fro_Tra_id = find((Note_Attr_KeyWords == 'from_track') ==1);
     Note_Attr_number = max(size(Note_Attr_KeyWords));
+    %创建存储cell
     Note_Martix = cell(Note_Nodes.getLength, Note_Attr_number);
     note_available_count = 1;
     for i = 0:Note_Nodes.getLength - 1
@@ -75,29 +78,45 @@ function status = Read_Xml(XML_file_addr)
                 value = str2double(value);
             else
                 value = string(value);
-                value = value.replace('Left', 'L');
-                value = value.replace('Right', 'R');
-                value = value.replace('short', 'shot');
-                value = value.replace('Middle', 'MD');
             end
             Note_Martix{note_available_count, j} = value;
         end  
-        if (Note_Martix{note_available_count, Not_Typ_id} == "shot") ||...
-                (Note_Martix{note_available_count, Not_Typ_id} ==  "slip")
-            Note_Martix{note_available_count, End_Bar_id} = Note_Martix{note_available_count, Str_Bar_id};
-            Note_Martix{note_available_count, End_Pos_id} = Note_Martix{note_available_count, Str_Pos_id};
-        end    
-        if Note_Martix{note_available_count, Not_Typ_id} ==  "slip"
-            Note_Martix{note_available_count, Fro_Tra_id} = Note_Martix{note_available_count, End_Tra_id};
-        end
         note_available_count = note_available_count + 1;
     end
     Note_Martix = Note_Martix(1:note_available_count - 1, :);
 
     %To Table
     Note_Martix_table = cell2table(Note_Martix, 'VariableNames', Note_Attr_KeyWords_cell);
+    
+    %针对不同类型处理
     if ~isempty(find((Note_Martix_table.EndArea == "") == 0))
         Info_Martix_table.ModeType = "PinBall";
+        
+        % EndBar = Bar;EndPos = Pos;
+        Note_Martix_table.EndBar(Note_Martix_table.note_type == "PinballSingle")=...
+            Note_Martix_table.Bar(Note_Martix_table.note_type == "PinballSingle");
+        Note_Martix_table.EndPos(Note_Martix_table.note_type == "PinballSingle")=...
+            Note_Martix_table.Pos(Note_Martix_table.note_type == "PinballSingle");
+        
+        Note_Martix_table.EndBar(Note_Martix_table.note_type == "PinballSlip")=...
+            Note_Martix_table.Bar(Note_Martix_table.note_type == "PinballSlip");
+        Note_Martix_table.EndPos(Note_Martix_table.note_type == "PinballSlip")=...
+            Note_Martix_table.Pos(Note_Martix_table.note_type == "PinballSlip");
+        
+        % 排除PinballSeries中的Son
+        Son_Note_Id = str2double(Note_Martix_table.Son(~isnan(str2double(Note_Martix_table.Son))));
+        for t = 1:max(size(Son_Note_Id))
+            if Note_Martix_table.note_type(Son_Note_Id(t)) ~= "PinballSlip"
+                continue;
+            else
+                Note_Martix_table.note_type(Son_Note_Id(t)) = "PinballSlipX2";
+                %Son的From为主的Target
+                Note_Martix_table.from_track(Son_Note_Id(t)) = Note_Martix_table.EndArea(Note_Martix_table.Son == string(Son_Note_Id(t)));
+            end
+        end
+        % Pinball的Target为EndArea;From为NULL
+        Note_Martix_table.target_track = Note_Martix_table.EndArea;
+        
     elseif XML_file.getElementsByTagName('ScreenPos').getLength > 2
         Info_Martix_table.ModeType = "Bubble";
         Note_Martix_table.Pos = Note_Martix_table.BeatPos; 
@@ -106,26 +125,42 @@ function status = Read_Xml(XML_file_addr)
             Note_Martix_table.Pos(Note_Martix_table.note_type == 0);
         Note_Martix_table.EndBar(Note_Martix_table.note_type == 0) = ...
             Note_Martix_table.Bar(Note_Martix_table.note_type == 0);
-        
-        Note_Martix_table.note_type = string(Note_Martix_table.note_type);
-        Note_Martix_table.EndPos = str2double(Note_Martix_table.EndPos);
-        Note_Martix_table.EndBar = str2double(Note_Martix_table.EndBar);
-        Note_Martix_table.from_track(:) = "Null";
-        Note_Martix_table.target_track = Note_Martix_table.from_track;
+
     else
         Info_Martix_table.ModeType = "Idol";
+        % EndBar = Bar;EndPos = Pos;
+        Note_Martix_table.EndBar(Note_Martix_table.note_type == "short")=...
+            Note_Martix_table.Bar(Note_Martix_table.note_type == "short");
+        Note_Martix_table.EndPos(Note_Martix_table.note_type == "short")=...
+            Note_Martix_table.Pos(Note_Martix_table.note_type == "short");
+        
+        Note_Martix_table.EndBar(Note_Martix_table.note_type == "slip")=...
+            Note_Martix_table.Bar(Note_Martix_table.note_type == "slip");
+        Note_Martix_table.EndPos(Note_Martix_table.note_type == "slip")=...
+            Note_Martix_table.Pos(Note_Martix_table.note_type == "slip");
     end
+    
+    % 避免全滑条靠北图str2double出错
+    Note_Martix_table.EndBar = string(Note_Martix_table.EndBar);
+    Note_Martix_table.EndPos = string(Note_Martix_table.EndPos);
+    Note_Martix_table.EndBar = str2double(Note_Martix_table.EndBar);
+    Note_Martix_table.EndPos = str2double(Note_Martix_table.EndPos);
+    % 确保note_type为string
+    Note_Martix_table.note_type = string(Note_Martix_table.note_type);
+        
     %Raw_data
     Map.Info = Info_Martix_table;
     Map.Notes = Note_Martix_table;
     clearvars -except Map;
-    
+    Note_Martix_table_out = table;
     Note_Martix_table_out.Pos = (Map.Notes.Bar-1)*4*8 + Map.Notes.Pos/2;
     Note_Martix_table_out.EndPos = (Map.Notes.EndBar-1)*4*8 + Map.Notes.EndPos/2;
     Note_Martix_table_out.note_type = Map.Notes.note_type;
     Note_Martix_table_out.from_track = Map.Notes.from_track;
     Note_Martix_table_out.target_track = Map.Notes.target_track;
     
+    Map_out.Info = Map.Info;
+    Map_out.Notes = Note_Martix_table_out;
     file_save_name = sprintf('%s-%s-%s-%s.txt', Map.Info.BgmId,...
         Map.Info.EnterTimeAdjust, Map.Info.BPM,...
         Map.Info.Title);
