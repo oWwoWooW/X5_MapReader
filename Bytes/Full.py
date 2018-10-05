5c75e072942cb0529fd3a751b6c480ad7bc0dee5
oWwoWooW <oWwoWooW@users.noreply.github.com>
2018-10-05 21:04:27 +0800

    实现文件输出
    实现文件输出
    测试集全部通过

--- Bytes/Full.py
+++ Bytes/Full.py
@@ -1,16 +1,18 @@
 # coding=utf-8
 
-import binascii, os, string
+import binascii, os, string, re
 from Bytes_Bubble import Get_Information
 
 FileNameList = []
 Folder_addr = r'F:\0518\All_level\Bytes_bubble'
+Save_Folder_addr = 'C:/Out/'
 for abs_dir, sub_dir, file_names in os.walk(Folder_addr):
     for file_name in file_names:
         if string.find(file_name, '.bytes') != -1:
             FileNameList.append('%s%s' % (abs_dir+'\\', file_name))
         else:
             continue
+
 i = 0
 for file_addr in FileNameList:
     # file_addr = r'F:\0518\All_level\Bytes\bubble_200004.xml.bytes'
@@ -33,4 +35,13 @@ for file_addr in FileNameList:
                      % (item['Note_type'], item['Start_Total_Pos'], item['End_Total_Pos'])
     i += 1
     print('%i/%i' % (i, FileNameList.__len__()))
+    try:
+        # 文件名规范化
+        title = re.sub(re.compile('[<>/\\|:"*,?]', re.S), "", Map_out['Info']['Title'])
+        f_save = open('%s%s.txt' % (Save_Folder_addr.decode('utf8'), title.decode('utf8')), 'w')
+        f_save.write(buf_info)
+        f_save.write(buf_notes)
+        f_save.close()
+    except Exception:
+        print('ws')
 print 'OK'
